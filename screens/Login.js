0087ee5707c16d840eaa21b973d3d2b7216b935f
krishna-s11 <krishnasaxena69@gmail.com>
2023-04-30 16:13:31 +0530

    login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Login = () => {
   return (
-    <View style={styles.container}>
-      <Text>This is Login</Text>
-      {/* <StatusBar style="auto" /> */}
+    <View>
+      <View style={styles.header}>
+        <Image
+          source={require("../assets/jiit_logo.png")}
+          style={styles.logo}
+        />
+      </View>
+      <View style={styles.login_container}>
+        <Text
+          style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: 20,
+            fontSize: 30,
+            // fontFamily: "Roboto-Regular",
+          }}
+        >
+          Login
+        </Text>
+      </View>
     </View>
   );
 };
@@ -12,10 +29,27 @@ const Login = () => {
 export default Login;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  header: {
+    backgroundColor: "#000",
+    width: "100%",
+    height: "40%",
     justifyContent: "center",
+    alignItems: "stretch",
+  },
+  login_container: {
+    backgroundColor: "#ffffff",
+    width: "100%",
+    height: "100%",
+    borderTopLeftRadius: 60,
+    marginTop: -100,
+    padding: 20,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: 60,
   },
 });
