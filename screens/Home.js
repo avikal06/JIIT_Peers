f236b4777b8587bd8d24fe406490fd02e77c1abe
avikal06 <haravikal06@gmail.com>
2023-04-30 16:13:50 +0530

    home

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,38 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-
+import { Button, StyleSheet, Text, View,ImageBackground } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Spacing from "../constants/Spacing";
+import FontSize from "../constants/FontSize";
+import Spacing from "../constants/Spacing";
+const {height} =Dimensions.get("window")
 const Home = ({ navigation }) => {
   return (
+    <SafeAreaView>
     <View style={styles.container}>
-      <Text>This is home</Text>
+      <ImageBackground style ={{
+        height: height/4,
+      }}
+      resizeMode="contain"
+      source ={require("../assets/images/jiit.png")}/>
       {/* <StatusBar style="auto" /> */}
+      <View style={{
+        paddingHorizontal:Spacing*4,
+        paddingTop : Spacing*4,
+      }}>
+        <Text
+        style={{
+          fontSize
+        }}></Text>
       <Button
         title="Login"
         onPress={() => {
           navigation.navigate("Login");
         }}
       />
+      <Button title ="Login" onPress ={() => { navigation.navigate("Login");}}/>
+      </View>
     </View>
+    </SafeAreaView>
   );
 };
 
